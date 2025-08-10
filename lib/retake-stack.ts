8019220a9f7a9dbb695ea9eb9f810864d742ef77
sea <kabalsky@protonmail.com>
2025-08-10 10:11:02 +0200

    fix cors

--- lib/retake-stack.ts
+++ lib/retake-stack.ts
@@ -2,30 +2,18 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
-
-import * as iam from "aws-cdk-lib/aws-iam";
-
-import * as sqs from "aws-cdk-lib/aws-sqs";
-
-import * as path from "path";
-import { Queue } from "aws-cdk-lib/aws-sqs";
-import { Subscription, Topic } from "aws-cdk-lib/aws-sns";
-
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
-import { Code, Runtime } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-
-;
-import * as s3n from "aws-cdk-lib/aws-s3-notifications";
+import { RemovalPolicy, aws_s3 as s3, aws_s3_deployment as s3deploy, aws_lambda as lambda, aws_apigateway as apigateway, aws_dynamodb as ddb, aws_sns as sns, aws_sns_subscriptions as subs } from 'aws-cdk-lib';
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
-import { Stack, StackProps, RemovalPolicy, aws_s3 as s3, aws_s3_deployment as s3deploy, aws_lambda as lambda, aws_apigateway as apigateway, aws_dynamodb as ddb, aws_sns as sns, aws_sns_subscriptions as subs } from 'aws-cdk-lib';
-
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import path from 'path';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class RetakeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-     const websiteBucket = new Bucket(this, "StaticWebSiteBucket", {
+    const websiteBucket = new Bucket(this, "StaticWebSiteBucket", {
       websiteIndexDocument: "index.html",
       publicReadAccess: true,
       blockPublicAccess: new BlockPublicAccess({
@@ -34,7 +22,7 @@ export class RetakeStack extends cdk.Stack {
         ignorePublicAcls: false,
         restrictPublicBuckets: false,
       }),
-       removalPolicy: cdk.RemovalPolicy.DESTROY,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
 
@@ -65,30 +53,37 @@ export class RetakeStack extends cdk.Stack {
     const topic = new sns.Topic(this, 'FavoriteCatTopic');
     topic.addSubscription(new subs.EmailSubscription('seyhan_akifov@yahoo.com'));
 
-    const saveCatFunction = new lambda.Function(this, 'SaveCatFunction', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'saveCat.handler',
-      code: lambda.Code.fromAsset('src'),
+    const saveCatFunction = new NodejsFunction(this, "PostTableFunction", {
+      entry: path.join(__dirname, "../src/saveCat.ts"),
+      handler: "handler",
+      runtime: Runtime.NODEJS_20_X,
       environment: {
         TABLE_NAME: table.tableName,
         TOPIC_ARN: topic.topicArn,
       },
     });
-
+    
     table.grantReadWriteData(saveCatFunction);
     topic.grantPublish(saveCatFunction);
 
     const api = new apigateway.RestApi(this, 'FavoriteCatApi', {
       defaultCorsPreflightOptions: {
+
         allowOrigins: apigateway.Cors.ALL_ORIGINS,
+
         allowMethods: apigateway.Cors.ALL_METHODS,
+
       },
     });
 
-    api.root
-      .addResource('saveCat')
-      .addMethod('POST', new apigateway.LambdaIntegration(saveCatFunction));
+    const catApi = api;
+    const catapiressourse = catApi.root.addResource("saveCat");
 
+    const orderapipost = catapiressourse.addMethod(
+      "POST",
+      new LambdaIntegration(saveCatFunction, { proxy: true })
+    );
+    
     new cdk.CfnOutput(this, 'WebsiteURL', {
       value: websiteBucket.bucketWebsiteUrl,
     });
--- src/saveCat.ts
+++ src/saveCat.ts
@@ -11,36 +11,52 @@ const TOPIC_ARN = process.env.TOPIC_ARN!;
 
 export const handler = async (event: APIGatewayProxyEvent) => {
     const { catId, savedUrl } = JSON.parse(event.body || '{}');
-
+    const itemUID = uuidv4();
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST',
+    };
     console.log(JSON.stringify(event));
 
-    if (!catId || !savedUrl) {
+    try {
+
+        if (!catId || !savedUrl) {
+            return {
+                statusCode: 400,
+                headers,
+                body: JSON.stringify({ message: 'Missing catId or savedUrl' }),
+            };
+        }
+        
+        await ddbClient.send(new PutItemCommand({
+            TableName: TABLE_NAME,
+            Item: {
+                PK: { S: `FAVORITECAT#${itemUID.toString()}` },
+                SK: { S: `METADATA#${itemUID.toString()}` },
+                catId: { S: catId },
+                savedUrl: { S: savedUrl },
+                updatedAt: { S: new Date().toISOString() },
+            }
+        }));
+
+        await snsClient.send(new PublishCommand({
+            TopicArn: TOPIC_ARN,
+            Subject: 'New Favorite Cat 🐱',
+            Message: `Lydia has a new favorite cat!\n\nID: ${catId}\nURL: ${savedUrl}`,
+        }));
+
         return {
-            statusCode: 400,
-            body: JSON.stringify({ message: 'Missing catId or savedUrl' }),
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({ message: 'Cat saved and Lydia notified.' }),
+        };
+    } catch (error) {
+        console.error('Error saving cat:', error);
+        return {
+            statusCode: 500,
+            headers,
+            body: JSON.stringify({ message: 'Internal server error ' + error }),
         };
     }
-    const itemUID = uuidv4();
-
-    await ddbClient.send(new PutItemCommand({
-        TableName: TABLE_NAME,
-        Item: {
-            PK: { S: `FAVORITECAT#${itemUID.toString()}` },
-            SK: { S: `METADATA#${itemUID.toString()}` },
-            catId: { S: catId },
-            savedUrl: { S: savedUrl },
-            updatedAt: { S: new Date().toISOString() },
-        }
-    }));
-
-    await snsClient.send(new PublishCommand({
-        TopicArn: TOPIC_ARN,
-        Subject: 'New Favorite Cat 🐱',
-        Message: `Lydia has a new favorite cat!\n\nID: ${catId}\nURL: ${savedUrl}`,
-    }));
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Cat saved and Lydia notified.' }),
-    };
 };
